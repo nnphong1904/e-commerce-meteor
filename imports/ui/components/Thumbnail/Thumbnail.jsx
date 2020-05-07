3cab9f28d12966bb82288757c9d620982d1d7d51
nnphong1904 <nnphong1904@gmail.com>
2020-05-07 23:04:33 +0700

    fix the avt name

--- imports/ui/components/Thumbnail/Thumbnail.jsx
+++ imports/ui/components/Thumbnail/Thumbnail.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import background from '../../assets/image/thumbnail_background.jpg';
 import './Thumbnail.css';
 const Thumbnail = ()=>{
-  console.log(background);
   const content = (
     <div className="thumbnail-container">
       <div className="thumbnail-title">OUTFIT OF THE WEEK</div>
