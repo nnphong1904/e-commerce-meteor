8a158fbbc32b06ebdee5c52c229994f270a1d351
nnphong1904 <nnphong1904@gmail.com>
2020-05-07 23:32:44 +0700

    complete UI for home-page

--- imports/ui/components/Thumbnail/Thumbnail.jsx
+++ imports/ui/components/Thumbnail/Thumbnail.jsx
@@ -3,10 +3,30 @@ import background from '../../assets/image/thumbnail_background.jpg';
 import './Thumbnail.css';
 const Thumbnail = ()=>{
   const content = (
-    <div className="thumbnail-container">
-      <div className="thumbnail-title">OUTFIT OF THE WEEK</div>
-      <button>Shop Now</button>
-    </div>
+   <>
+      <div className="thumbnail-container">
+        <div className="thumbnail-title">OUTFIT OF THE WEEK</div>
+        <button className="shop-now-btn">Shop Now</button>
+      </div>
+      <div className="filter-container">
+        <div className="filter-card">
+          <div className="card-title">Men</div>
+          <button className="shop-now-btn">Shop Now</button>
+        </div>
+        <div className="filter-card">
+          <div className="card-title">Men</div>
+          <button className="shop-now-btn">Shop Now</button>
+        </div>
+        <div className="filter-card">
+          <div className="card-title">Men</div>
+          <button className="shop-now-btn">Shop Now</button>
+        </div>
+        <div className="filter-card">
+          <div className="card-title">Men</div>
+          <button className="shop-now-btn">Shop Now</button>
+        </div>
+      </div>
+   </>
   )   
   return content;
 }
