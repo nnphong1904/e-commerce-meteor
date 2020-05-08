7292882c603978f869f476c9cd14f8bd2f5ff84d
nnphong1904 <nnphong1904@gmail.com>
2020-05-08 10:23:54 +0700

    change import file name

--- imports/ui/lib/route.jsx
+++ imports/ui/lib/route.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {mount} from 'react-mounter';
 import {App} from '../App';
-import Thumbnail from '../components/Thumbnail/Homepage.jsx';
+import Homepage from '../components/Thumbnail/Homepage.jsx';
 FlowRouter.route('/',{
   name:'home',
   action(){
-    mount(({component})=><App component={component}/>,{component: <Thumbnail/>});
+    mount(({component})=><App component={component}/>,{component: <Homepage/>});
   }
 })
 FlowRouter.route('/products',{
