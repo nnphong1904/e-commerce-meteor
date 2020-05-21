7ead9f03c4360df4462df2e952a7417418836b9e
nnphong1904 <nnphong1904@gmail.com>
2020-05-21 22:14:16 +0700

    improve ux for select admin orders or product

--- imports/ui/components/AdminSidebar/AdminSidebar.jsx
+++ imports/ui/components/AdminSidebar/AdminSidebar.jsx
@@ -1,4 +1,4 @@
-import React, { createRef, useState } from 'react';
+import React, { createRef, useState, useEffect } from 'react';
 import './AdminSidebar.css';
 import Logo from '../../assets/image/logo.svg';
 import AdminSiteSelector from '../AdminSiteSelector/AdminSiteSelector.jsx';
@@ -12,21 +12,17 @@ const AdminSidebar = (props)=>{
   
   const siteSelectorRef = createRef();
   const [site, setSite] = useState('orders');
-  const didChangeSite = (e)=>{
-    if (site !== e.target.value.toLowerCase()){
-      console.log('uncheck')
-      setSite(e.target.value.toLowerCase());
-      return true;
-    }
-    return false;
-  }
+  useEffect(()=>{
+    siteSelectorRef.current.children[0].children[0].checked = true;
+    // siteSelectorRef.current.children[0].children[1].src = CartOrange;
+  },[])
 
   const content = (
     <div className="admin-sidebar-container">
         <img className="sidebar-logo" src={Logo} />
-        <form className="select-site-container">
-            <AdminSiteSelector onClickFunction={{didChangeSite}} title="Orders" checkedLogo={CartOrange} logo={Cart}/>
-            <AdminSiteSelector onClickFunction={{didChangeSite}} title="Products" checkedLogo={ProductLogoOrange} logo={ProductLogo}/>
+        <form ref={siteSelectorRef} className="select-site-container">
+            <AdminSiteSelector onClickFunction={setSite} checkedInitial={true} currentSite={site} title="Orders" checkedLogo={CartOrange} logo={Cart}/>
+            <AdminSiteSelector onClickFunction={setSite} currentSite={site}  title="Products" checkedLogo={ProductLogoOrange} logo={ProductLogo}/>
         </form>
     </div>
   );
