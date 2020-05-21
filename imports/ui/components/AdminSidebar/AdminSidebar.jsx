096c4e62d62cdc7d530a7262c177519d2f417f7d
nnphong1904 <nnphong1904@gmail.com>
2020-05-21 17:59:41 +0700

    build ux for select site admin

--- imports/ui/components/AdminSidebar/AdminSidebar.jsx
+++ imports/ui/components/AdminSidebar/AdminSidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef, useState } from 'react';
 import './AdminSidebar.css';
 import Logo from '../../assets/image/logo.svg';
 import AdminSiteSelector from '../AdminSiteSelector/AdminSiteSelector.jsx';
@@ -9,16 +9,24 @@ import ProductLogoOrange from '../../assets/image/products-orange.svg';
 
 const AdminSidebar = (props)=>{
 
-  const markChecked = ()=>{
-
+  
+  const siteSelectorRef = createRef();
+  const [site, setSite] = useState('orders');
+  const didChangeSite = (e)=>{
+    if (site !== e.target.value.toLowerCase()){
+      console.log('uncheck')
+      setSite(e.target.value.toLowerCase());
+      return true;
+    }
+    return false;
   }
 
   const content = (
     <div className="admin-sidebar-container">
         <img className="sidebar-logo" src={Logo} />
         <form className="select-site-container">
-            <AdminSiteSelector title="Orders" checkedLogo={CartOrange} logo={Cart}/>
-            <AdminSiteSelector title="Products" checkedLogo={ProductLogoOrange} logo={ProductLogo}/>
+            <AdminSiteSelector onClickFunction={{didChangeSite}} title="Orders" checkedLogo={CartOrange} logo={Cart}/>
+            <AdminSiteSelector onClickFunction={{didChangeSite}} title="Products" checkedLogo={ProductLogoOrange} logo={ProductLogo}/>
         </form>
     </div>
   );
--- imports/ui/components/AdminSiteSelector/AdminSiteSelector.jsx
+++ imports/ui/components/AdminSiteSelector/AdminSiteSelector.jsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect, createRef } from 'react';
 import './AdminSiteSelector.css';
 
-const AdminSiteSelector = ({onClickFunction = ()=>{}, checkedLogo='' ,title='', logo=''})=>{
+const AdminSiteSelector = ({onClickFunction = ()=>{} ,checkedLogo='' ,title='', logo=''})=>{
   
   const ref = createRef();
   const [checked, setChecked] = useState(false);
-  const changeColorLogo = ()=>{
-   setChecked(oldChecked => !oldChecked);
+  const changeColorLogo = (e)=>{
+   
   }
   const content =(  
     <label   
         className="admin-site-selector-holder">
-      <input onClick={(e)=>changeColorLogo()}  name="site-admin" type='radio' className="site-checker"/> 
-         <img ref={ref} className="site-logo" src={logo}/>  
+      <input onClick={(e)=>{
+       
+      }} 
+      value={title}  
+      name="site-admin" type='radio' className="site-checker"/> 
+         <img ref={ref}  className="site-logo" src={logo}/>  
       <div className="site-title">{title}</div>  
     </label>
   );
