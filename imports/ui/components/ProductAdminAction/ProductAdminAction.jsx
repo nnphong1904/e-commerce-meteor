102d44242fb06dab38a5e6dc0d2c5e2539ddb32a
nnphong1904 <nnphong1904@gmail.com>
2020-05-29 14:43:28 +0700

    error hanlder for remove product when server is down

--- imports/ui/components/ProductAdminAction/ProductAdminAction.jsx
+++ imports/ui/components/ProductAdminAction/ProductAdminAction.jsx
@@ -5,21 +5,7 @@ import Remove from '../../assets/image/remove.svg';
 import axios from 'axios';
 const ProductAdminAction = ({updateProductsList=()=>{} ,turnOnEditForm = ()=>{} ,currentList=[], oldList=[] ,productIndex=0 ,productId='', updateCurrentPage=()=>{} ,updateNumberOfPage=()=>{} ,currentPage=1})=>{
   const removeProduct = (id, index)=>{
-    const newDisplayingProductsList = [...currentList.slice(0, index),
-                             ...currentList.slice(index+1)  
-                            ];
-    const originIndex = index + (currentPage -1)*6;
-    const newProductsList = [...oldList.slice(0,originIndex), ...oldList.slice(originIndex+1)];
-    if (newDisplayingProductsList.length === 0){
-      updateNumberOfPage(old => old -1);
-      updateProductsList.setCurrentProductsList([...oldList.slice((currentPage-2)*6,(currentPage-2)*6+6 )]);
-      updateCurrentPage(currentPage-1);
-    }
-    else{
-      updateProductsList.setCurrentProductsList([...newDisplayingProductsList]);
-    }
-    
-    updateProductsList.setOldProductsList([...newProductsList]);
+
     const URL = `http://localhost:4000/api/v1/product/${id}`;
     Meteor.call('getHashedToken', (err, result)=>{
       // console.log(result);
@@ -31,9 +17,24 @@ const ProductAdminAction = ({updateProductsList=()=>{} ,turnOnEditForm = ()=>{} 
       })
       .then(res=>{
         console.log(res);
+        const newDisplayingProductsList = [...currentList.slice(0, index),
+          ...currentList.slice(index+1)  
+         ];
+        const originIndex = index + (currentPage -1)*6;
+        const newProductsList = [...oldList.slice(0,originIndex), ...oldList.slice(originIndex+1)];
+        if (newDisplayingProductsList.length === 0){
+        updateNumberOfPage(old => old -1);
+        updateProductsList.setCurrentProductsList([...oldList.slice((currentPage-2)*6,(currentPage-2)*6+6 )]);
+        updateCurrentPage(currentPage-1);
+        }
+        else{
+        updateProductsList.setCurrentProductsList([...newDisplayingProductsList]);
+        }
+
+        updateProductsList.setOldProductsList([...newProductsList]);
       })
       .catch(err=>{
-        console.log(err);
+       window.alert('Sorry server is down!!');
       })
     });
     
